6d75c3e3c050e377554fdad24d30cbb83c28579e
ralfvivaldo <ralf.vivaldo@gmail.com>
2024-05-25 04:35:02 +0700

    MESC PANAORAMA FILE

---
+++ data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama_mesc---skp---render_2024-05-24-22-25-34",
+      "name": "Panorama_MESC - SKP - RENDER_2024-05-24-22-25-34",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
